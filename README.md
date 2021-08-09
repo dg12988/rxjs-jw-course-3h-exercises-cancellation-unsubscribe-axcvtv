8eb883cce070e9c95aa37411d5a442a0970677d5
dg12988 <doug.goldberg@me.com>
2021-08-09 12:44:33 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# rxjs-jw-course-3h-exercises-cancellation-unsubscribe-axcvtv
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/rxjs-jw-course-3h-exercises-cancellation-unsubscribe-axcvtv)
---
+++ index.ts
@@ -0,0 +1,15 @@
+import { observable, Observable } from 'rxjs';
+
+const observable$ = new Observable<string>(subscriber => {
+  setInterval(() => {
+    subscriber.next('next interval');
+  }, 1000);
+});
+
+const subscription = observable$.subscribe({
+  next: value => console.log(value),
+  error: err => console.log(err.message),
+  complete: () => console.log('Completed')
+});
+
+setTimeout(() => subscription.unsubscribe(), 7000);
